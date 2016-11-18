7b25edb091d3830709ea2f3a381b99e3a3ccfd63
Ionar <irm@ionar.com.br>
2016-11-18 16:43:30 -0200

    config deploy with seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,50 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Clinic.create!([
+  {fantasy_name: "Radiodôntica - São Miguel do Oeste", corporate_name: "MSS", cnpj: "dfdf234234", technical_manager: "5234234", address: "234234", cep: "234234", city: "234234", province_id: 20, phone: "234234"},
+  {fantasy_name: "Radiodôntica - Chapecó", corporate_name: "asdfaf", cnpj: "adfadf", technical_manager: "adfadf", address: "asdfadf", cep: "sdfasdf", city: "adaf", province_id: 2, phone: "asfdaf"},
+  {fantasy_name: "Radiodôntica - Itapiranga", corporate_name: "MSS ", cnpj: "656465464", technical_manager: "Dr. Vilmar Siviero", address: "Av. Beira Rio", cep: "89900-000", city: "Itapiranga", province_id: 1, phone: "6464654"},
+  {fantasy_name: "Radiodôntica Cedro", corporate_name: "MSS tal", cnpj: "64684648", technical_manager: "Dr. Vilmar Siviero", address: "Rua Rio", cep: "89905-000", city: "São José do Cedro", province_id: 1, phone: "684684684"}
+])
+Dentist.create!([
+  {name: "Dr. Vilmar Cleoci Siviero", register: "CRO 3628/SC", email: "radiodontica@radiodontica.com.br", phone: "", notes: "Requer que envie a doc para o consultório."},
+  {name: "Dr. Ariovaldo", register: "CRO 6666", email: "", phone: "", notes: ""}
+])
+Exam.create!([
+  {name: "PAN"},
+  {name: "Tomografia"},
+  {name: "Documentação"}
+])
 
-genders = Gender.create([{ name: 'Masculino'}, { name: 'Feminino' }])
+Gender.create!([
+  {name: "Masculino"},
+  {name: "Feminino"}
+])
 
+Province.create!([
+  {name: "Acre", acronym: "AC"},
+  {name: "Alagoas", acronym: "AL"},
+  {name: "Amapá", acronym: "AP"},
+  {name: "Amazonas", acronym: "AM"},
+  {name: "Bahia", acronym: "BA"},
+  {name: "Ceará", acronym: "CE"},
+  {name: "Distrito Federal", acronym: "DF"},
+  {name: "Espírito Santo", acronym: "ES"},
+  {name: "Goiás", acronym: "GO"},
+  {name: "Maranhão", acronym: "MA"},
+  {name: "Mato Grosso", acronym: "MT"},
+  {name: "Mato Grosso do Sul", acronym: "MS"},
+  {name: "Minas Gerais", acronym: "MG"},
+  {name: "Pará", acronym: "PA"},
+  {name: "Paraíba", acronym: "PB"},
+  {name: "Paraná", acronym: "PR"},
+  {name: "Pernambuco", acronym: "PE"},
+  {name: "Piauí", acronym: "PI"},
+  {name: "Rio de Janeiro", acronym: "RJ"},
+  {name: "Rio Grande do Norte", acronym: "RN"},
+  {name: "Rio Grande do Sul", acronym: "RS"},
+  {name: "Rondônia", acronym: "RO"},
+  {name: "Roraima", acronym: "RR"},
+  {name: "Santa Catarina", acronym: "SC"},
+  {name: "São Paulo", acronym: "SP"},
+  {name: "Sergipe", acronym: "SE"},
+  {name: "Tocantins", acronym: "TO"}
+])
