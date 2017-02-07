91031bf6131d5a5dee46461971e818af23b38df0
Ionar <irm@ionar.com.br>
2017-02-07 11:55:37 -0200

    disable validations temp

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -2,7 +2,7 @@ class Patient < ActiveRecord::Base
   belongs_to :province
   has_many :attends
 
-  validates :name, :cpf, :rg, :email, uniqueness: true
-  validates :name, :phone, presence: true
+  #validates :name, :cpf, :rg, :email, uniqueness: true
+  #validates :name, :phone, presence: true
 
 end
