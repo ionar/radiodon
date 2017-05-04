0b1b65acb572a82ac0566908d5b9808656d44c1c
Ionar <irm@ionar.com.br>
2017-05-04 14:00:05 -0300

    fix dependent relationship

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,6 +1,6 @@
 class Patient < ActiveRecord::Base
   belongs_to :province
-  has_many :attends
+  has_many :attends, :dependent => :delete_all
 
   #validates :name, :cpf, :rg, :email, uniqueness: true
   validates :name, :phone, presence: true
