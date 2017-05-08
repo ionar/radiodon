742b7c1b46f2867be77b7bf10b5f9f299e9c31e5
Ionar <irm@ionar.com.br>
2017-05-08 17:23:59 -0300

    fix name migration

--- db/migrate/20170508184752_remove_foreignkey_from_patients.rb
+++ db/migrate/20170508184752_remove_foreignkey_from_patients.rb
@@ -1,4 +1,4 @@
-class RemoveForeignKeyFromPatients < ActiveRecord::Migration
+class RemoveForeignkeyFromPatients < ActiveRecord::Migration
   def change
     #remove_reference :patients, :province, index: true, foreign_key: true
     remove_foreign_key :patients, :provinces
