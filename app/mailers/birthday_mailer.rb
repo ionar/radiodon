baaacf9f80f51a687d73fadfed1cd94491621f5b
Ionar <irm@ionar.com.br>
2017-07-18 20:03:09 -0300

    fix bugs

--- app/mailers/birthday_mailer.rb
+++ app/mailers/birthday_mailer.rb
@@ -4,10 +4,10 @@ class BirthdayMailer < ApplicationMailer
   def greetings_email(patient)
     @patient = patient
     @url  = 'http://www.radiodontica.com.br'
-    mail(to: @patient.email, subject: 'Feliz Aniversário')
+    mail(to: @patient.email, subject: 'Feliz Aniversario')
   end
 end
 
 #Entregar via console
-#@patient = Patient.find(47770)
+#@patient = Patient.find(10787)
 #BirthdayMailer.greetings_email(@patient).deliver
